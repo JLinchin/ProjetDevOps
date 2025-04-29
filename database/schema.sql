03961dc9bf99c029bbf583d3e38b3537ceec5e03
Joseph Minchin <joseph.minchin@ecole-hexagone.com>
2025-04-29 15:13:25 +0200

    Test pour mathéo

--- database/schema.sql
+++ database/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
+    test varchar,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
